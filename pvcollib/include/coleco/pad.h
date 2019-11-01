bf2df13cec66a7be74ccf9ffd9b10bb0db9cb3bf
Alekmaul <alekmaul@portabledev.com>
2019-11-01 09:41:32 +0100

    Initial commit

---
+++ pvcollib/include/coleco/pad.h
@@ -0,0 +1,97 @@
+/*---------------------------------------------------------------------------------
+
+	Generic pad & spinner functions.
+
+	Copyright (C) 2018-2019
+		Alekmaul
+
+	This software is provided 'as-is', without any express or implied
+	warranty.  In no event will the authors be held liable for any
+	damages arising from the use of this software.
+
+	Permission is granted to anyone to use this software for any
+	purpose, including commercial applications, and to alter it and
+	redistribute it freely, subject to the following restrictions:
+
+	1.	The origin of this software must not be misrepresented; you
+		must not claim that you wrote the original software. If you use
+		this software in a product, an acknowledgment in the product
+		documentation would be appreciated but is not required.
+	2.	Altered source versions must be plainly marked as such, and
+		must not be misrepresented as being the original software.
+	3.	This notice may not be removed or altered from any source
+		distribution.
+
+
+---------------------------------------------------------------------------------*/
+
+/*! \file pad.h
+    \brief coleco generic pad & spinner support.
+*/
+
+#ifndef COL_PAD_H
+#define COL_PAD_H
+
+#include <coleco/coltypes.h>
+
+#define	PAD_UP			1
+#define	PAD_RIGHT		2
+#define	PAD_DOWN		4
+#define	PAD_LEFT		8
+#define	PAD_FIRE4		16
+#define	PAD_FIRE3		32
+#define	PAD_FIRE2		64
+#define	PAD_FIRE1		128
+
+#define	PAD_KEY0		0
+#define	PAD_KEY1		1
+#define	PAD_KEY2		2
+#define	PAD_KEY3		3
+#define	PAD_KEY4		4
+#define	PAD_KEY5		5
+#define	PAD_KEY6		6
+#define	PAD_KEY7		7
+#define	PAD_KEY8		8
+#define	PAD_KEY9		9
+#define	PAD_KEYSTAR		10
+#define	PAD_KEYSHARP	11
+#define	PAD_KEYNONE		15
+
+/**
+ *  \brief
+ *      PAD 1 and 2 values
+ */
+extern volatile joypad_1,joypad_2;
+
+/**
+ *  \brief
+ *      SPINNER 1 and 2 values
+ *		/!\ spinner 1 value is reversed
+ */
+extern volatile spinner_1,spinner_2;
+
+/**
+ *  \brief
+ *      KEYBOARD from PAD 1 and 2 values
+ */
+extern volatile keypad_1,keypad_2;
+
+/*! \fn pad_resetspin(void)
+	\brief reset spinners 1 & 2 values
+	\return nothing
+*/
+void pad_resetspin(void);
+
+/*! \fn pad_disablespin(void)
+	\brief enable spinners functions 
+	\return nothing
+*/
+void pad_disablespin(void);
+
+/*! \fn pad_enablespin(void)
+	\brief enable spinners functions to get their values
+	\return nothing
+*/
+void pad_enablespin(void);
+
+#endif
---
+++ pvcollib/source/pad.s
@@ -0,0 +1,67 @@
+;---------------------------------------------------------------------------------
+;
+;	Copyright (C) 2018-2019
+;		Alekmaul 
+;
+;	This software is provided 'as-is', without any express or implied
+;	warranty.  In no event will the authors be held liable for any
+;	damages arising from the use of this software.
+;
+;	Permission is granted to anyone to use this software for any
+;	purpose, including commercial applications, and to alter it and
+;	redistribute it freely, subject to the following restrictions:
+;
+;	1.	The origin of this software must not be misrepresented; you
+;		must not claim that you wrote the original software. If you use
+;		this software in a product, an acknowledgment in the product
+;		documentation would be appreciated but is not required.
+;	2.	Altered source versions must be plainly marked as such, and
+;		must not be misrepresented as being the original software.
+;	3.	This notice may not be removed or altered from any source
+;		distribution.
+;
+;---------------------------------------------------------------------------------
+	.module pvclpad
+
+	; global from external entries / code
+	.globl _spinner_1
+	.globl _spinner_2
+	.globl _spinner_enabled	
+
+	; global from this module
+	.globl _pad_disablespin
+	.globl _pad_enablespin
+	.globl _pad_resetspin
+	
+	.area _CODE
+		
+;---------------------------------------------------------------------------------
+; Here begin routines that can't be call from programs
+;---------------------------------------------------------------------------------
+
+;---------------------------------------------------------------------------------
+; Here begin routines that can be call from programs
+;---------------------------------------------------------------------------------
+_pad_disablespin:
+	push af
+	di
+	xor a
+	ld (_spinner_enabled),a
+	pop af
+	ret
+	
+_pad_enablespin:
+	push af
+	ei
+	or #255
+	ld (_spinner_enabled),a
+	pop af
+	ret
+
+_pad_resetspin:
+	push af
+	xor	a
+	ld (_spinner_1),a
+	ld (_spinner_2),a
+	pop af
+	ret

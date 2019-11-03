2a6a5955e58dcfa82395603afb2461772e8428ce
Alekmaul <alekmaul@portabledev.com>
2019-11-03 16:46:19 +0100

    Initial commit

---
+++ coleco-examples/graphics/plecompress/grafplecomp.h
@@ -0,0 +1,6 @@
+#ifndef _GRAFPLECMP_MAIN_
+#define _GRAFPLECMP_MAIN_
+
+#include <coleco.h>
+
+#endif
---
+++ coleco-examples/graphics/plecompress/zgfxs.h
@@ -0,0 +1,8 @@
+#ifndef _GRAFNOCMP_GFXS_
+#define _GRAFNOCMP_GFXS_
+
+#include <coleco.h>
+
+#include "ourvisiongfx.h"		// generetad by gfx2col
+
+#endif

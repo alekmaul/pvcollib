e4acc026414a7e495449b4b3c68642408ba4aede
Alekmaul <alekmaul@portabledev.com>
2019-11-09 07:10:54 +0100

    Change version

--- pvcollib/include/coleco/libversion.h
+++ pvcollib/include/coleco/libversion.h
@@ -2,9 +2,9 @@
 #define __PVCOLLIBVERSION_H__
 
 #define _PVCOLLIB_MAJOR_	1
-#define _PVCOLLIB_MINOR_	1
+#define _PVCOLLIB_MINOR_	2
 #define _PVCOLLIB_PATCH_	0
 
-#define _PVCOLLIB_STRING "PVCOLLIB Release 1.1.0"
+#define _PVCOLLIB_STRING "PVCOLLIB Release 1.2.0"
 
 #endif // __PVCOLLIBVERSION_H__

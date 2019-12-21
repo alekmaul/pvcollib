d3f6fc7eab8c62332afd6b43c5300bfdf87d2b8b
Alekmaul <alekmaul@portabledev.com>
2019-12-21 08:22:43 +0100

    Initial commit

---
+++ coleco-examples/graphics/mode1plecompress/mode1plecompress.c
@@ -0,0 +1,38 @@
+/*---------------------------------------------------------------------------------
+
+
+	Graphic mode 1 example with a bitmap 
+	-- alekmaul
+
+
+---------------------------------------------------------------------------------*/
+#include "mode1plecompress.h"
+
+#include "gfxs.h"									// to add definition of graphics
+
+//---------------------------------------------------------------------------------
+// The NMI routine. Gets called 50 or 60 times per second 
+// nothing to update for bitmap example
+void nmi (void) {
+}
+
+//---------------------------------------------------------------------------------
+void main (void) {
+	vdp_disablescr();
+
+	// put screen in mode 1 
+	vdp_setmode1txt();
+	
+	vdp_ple2vram (TILourvision, chrgen); // characters
+	vdp_putvram (coltab,COLourvision,32); // colors (not compressed as it is only 32 bytes)
+	vdp_ple2vram (MAPourvision, chrtab); // map
+	
+	vdp_enablescr();
+
+	// Wait for nothing :P
+	while(1) {
+		// Wait Vblank
+		vdp_waitvblank(1);
+	}
+	// startup module will issue a soft reboot if it comes here 
+}

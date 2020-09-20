b671ffe2c7874fdda7c1a25b89c902272d607fd6
Alekmaul <alekmaul@portabledev.com>
2020-09-20 10:19:11 +0200

    Update to V1.5

--- pvcollib/include/coleco/libversion.h
+++ pvcollib/include/coleco/libversion.h
@@ -2,9 +2,9 @@
 #define __PVCOLLIBVERSION_H__
 
 #define _PVCOLLIB_MAJOR_	1
-#define _PVCOLLIB_MINOR_	4
+#define _PVCOLLIB_MINOR_	5
 #define _PVCOLLIB_PATCH_	0
 
-#define _PVCOLLIB_STRING "PVCOLLIB Release 1.4.0"
+#define _PVCOLLIB_STRING "PVCOLLIB Release 1.5.0"
 
 #endif // __PVCOLLIBVERSION_H__

a5680e2ce82b2d9a0c0f2f157a1310dadb58bfa5
Alekmaul <alekmaul@portabledev.com>
2020-10-04 06:53:09 +0200

    Initial commit

---
+++ coleco-examples/graphics/backgrounds/dan3compress/ourvisiongfx.h
@@ -0,0 +1,13 @@
+#ifndef ourvisiongfx_INC_
+#define ourvisiongfx_INC_
+
+#define SZTILourvisiongfx 462
+#define SZCOLourvisiongfx 208
+#define SZMAPourvisiongfx 196
+
+extern const unsigned char TILourvisiongfx[];
+extern const unsigned char COLourvisiongfx[];
+extern const unsigned char MAPourvisiongfx[];
+
+
+#endif
